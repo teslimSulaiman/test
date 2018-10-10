4d2ebfcec990c42f1628969c11b807c1127ff5a9
teslim <teslimsulaiman@gmail.com>
2018-10-10 14:37:09 +0100

    magento test

---
+++ Teslim/Test/Controller/Adminhtml/CustomController.php
@@ -0,0 +1,9 @@
+<?php
+class Teslim_Test_Adminhtml_CustomController extends Mage_Adminhtml_Controller_Action
+{
+
+    public function indexAction()
+    {
+        echo "hello world";
+    }
+}
---
+++ Teslim/Test/Controller/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Teslim_Test_IndexController extends Mage_Core_Controller_Front_Action
+
+{
+	public function indexAction()
+    {
+        echo "Hello world";
+    }
+}
